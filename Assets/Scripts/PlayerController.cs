faa6b778c888900d45fd93dce3217bc7919d165e
Matt <johnston.matt.vt@gmail.com>
2022-10-13 16:17:43 -0400

    basic scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     public float gravity = -20;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    public bool ableToDoubleJump = true;
+    public Animator animator;
+    public Transform model;
     void Start()
     {
 
@@ -23,18 +26,35 @@ public class PlayerController : MonoBehaviour
     {
         float hInput = Input.GetAxis("Horizontal");
         direction.x = hInput * speed;
-
+        animator.SetFloat("speed", Mathf.Abs(hInput));
         bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
-
-
-        direction.y += gravity * Time.deltaTime;
+        animator.SetBool("isGrounded", isGrounded);
 
         if (isGrounded)
         {
+            direction.y = -1;
+            ableToDoubleJump = true;
+
             if (Input.GetButtonDown("Jump"))
             {
                 direction.y = jumpForce;
             }
+        }
+        else
+        {
+            direction.y += gravity * Time.deltaTime;
+            if (ableToDoubleJump & Input.GetButtonDown("Jump"))
+            {
+                animator.SetTrigger("doubleJump");
+                direction.y = jumpForce;
+                ableToDoubleJump = false;
+            }
+        }
+
+        if (hInput != 0)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
+            model.rotation = newRotation;
         }
         controller.Move(direction * Time.deltaTime);
     }
